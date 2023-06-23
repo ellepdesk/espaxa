61f69d19bd4b95e9d609852c6688561cee9d966f
Pelle van der Heide <pelle.vanderheide@eneco.com>
2023-06-23 22:10:28 +0200

    Lots 'o' fixes
    Prototype seems to work
    Switch (back) to arduino platform
    Use MQTT
    
    restore desired state on boot

--- src/espaxa.h
+++ src/espaxa.h
@@ -31,33 +31,45 @@ enum class AxaPosition{
   OPEN_2,
   OPEN_3,
   OPEN_FULL,
+  OPEN_MANUAL,
+  UNKNOWN,
 };
-const int poll_delay_ms = 60000;
 
 const int unlock_time_ms = 10000;
-const int lock_time_ms = 20000;
+const int lock_time_ms = 30000;
 const int move_time_ms = 50000;
 const int move_step_time_ms = move_time_ms / int(AxaPosition::OPEN_FULL);
 const int steps = int(AxaPosition::OPEN_FULL);
 
-class EspAxaCover :
+
+class EspAxaCover:
   public Component,
   public UARTDevice,
   public Cover {
-  protected:
-    uint32_t lastread = millis() - poll_delay_ms;
-    uint32_t last_state_change = millis();
-    // uint8_t device[64] = {0};
-    // uint8_t firmware[64] = {0};
-    AxaPosition desired_pos = AxaPosition::LOCKED;
-    AxaPosition current_pos = AxaPosition::LOCKED;
-    bool state_dirty = true;
+ private:
+  AxaPosition desired_pos;
+  AxaPosition current_pos;
+  uint32_t last_state_change = 0;
+
  public:
   EspAxaCover(UARTComponent *parent) :
     Component(),
     UARTDevice(parent),
     Cover()
-    {}
+    {
+      auto restored_state = restore_state_();
+      if (restored_state.has_value()){
+        auto position = restored_state.value().position;
+        desired_pos= normalize_position(position);
+        current_pos = desired_pos;
+        CoverOperation desired_operation = update_operation(desired_pos);
+        apply_operation(desired_operation);
+      }
+      else {
+        desired_pos = AxaPosition::LOCKED;
+        current_pos = desired_pos;
+      }
+    }
 
   void setup() override {
     ESP_LOGCONFIG("espaxa", "Setting up AXA UART...");
@@ -78,9 +90,38 @@ class EspAxaCover :
   }
 
   float denormalize_positon(AxaPosition pos){
+    if (pos == AxaPosition::OPEN_MANUAL)
+      return 1.0;
     float p = int(pos);
     float fp = p / (steps * 1.0);
     return fp;
+  }
+
+  const char * axa_command_to_string(AxaCommand cmd){
+    switch(cmd){
+      case AxaCommand::STATUS:
+        return "STATUS\r\n";
+      break;
+      case AxaCommand::OPEN:
+        return "OPEN\r\n";
+      break;
+      case AxaCommand::CLOSE:
+        return "CLOSE\r\n";
+      break;
+      case AxaCommand::STOP:
+        return "STOP\r\n";
+      break;
+      case AxaCommand::FIRMWARE:
+        return "FIRMWARE\r\n";
+      break;
+      case AxaCommand::DEVICE:
+        return "DEVICE\r\n";
+      break;
+      case AxaCommand::HELP:
+        return "?:\r\n";
+      break;
+    }
+    return "";
   }
 
   AxaCode send_cmd(AxaCommand cmd, uint8_t * buffer = NULL, size_t bufferlen = 0)
@@ -91,39 +132,15 @@ class EspAxaCover :
     for (int i =0; i< 10; i++)
       if(available())
       {
+        // Clear buffer
         uint8_t read_buf[128] = {0};
         read_array(read_buf, min(sizeof(read_buf), uint32_t(available())));
-        // ESP_LOGCONFIG("espaxa", "junk: %s", read_buf);
-
       }
 
     uint8_t read_buf[128] = {0};
-    const char * cmd_str = "";
-    switch(cmd){
-      case AxaCommand::STATUS:
-        cmd_str = "STATUS\r\n";
-      break;
-      case AxaCommand::OPEN:
-        cmd_str ="OPEN\r\n";
-      break;
-      case AxaCommand::CLOSE:
-        cmd_str = "CLOSE\r\n";
-      break;
-      case AxaCommand::STOP:
-        cmd_str = "STOP\r\n";
-      break;
-      case AxaCommand::FIRMWARE:
-        cmd_str = "FIRMWARE\r\n";
-      break;
-      case AxaCommand::DEVICE:
-        cmd_str = "DEVICE\r\n";
-      break;
-      case AxaCommand::HELP:
-        cmd_str = "?:\r\n";
-      break;
-    }
+    auto cmd_str = axa_command_to_string(cmd);
     write_str(cmd_str);
-    delay(50);
+    delay(200);
     read_array(read_buf, min(sizeof(read_buf), uint32_t(available())));
     size_t len = min(strlen((char*)read_buf), sizeof(read_buf));
 
@@ -151,7 +168,7 @@ class EspAxaCover :
         break;
       }
     }
-    ESP_LOGCONFIG("espaxa", "send_cmd(%i): %i", int(cmd), int(return_code));
+    ESP_LOGCONFIG("espaxa", "send_cmd(%s): %i", axa_command_to_string(cmd), int(return_code));
     return return_code;
   }
 
@@ -159,61 +176,60 @@ class EspAxaCover :
     // This will be called every time the user requests a state change.
     if (call.get_position().has_value()) {
       auto pos = *call.get_position();
-      desired_pos = normalize_position(pos);
+      auto position = normalize_position(pos);
+      CoverOperation desired_operation = update_operation(position);
+      apply_operation(desired_operation);
     }
     if (call.get_stop()) {
       desired_pos = current_pos;
-      // User requested cover stop
+      send_cmd(AxaCommand::STOP);
     }
-
-    // Only apply new state when idle
-    if (current_operation == COVER_OPERATION_IDLE)
-    {
-      update_desired_operation();
-    }
-    publish();
   }
 
-  void update_desired_operation()
+  CoverOperation update_operation(AxaPosition pos)
   {
+    desired_pos = pos;
     CoverOperation desired_operation = COVER_OPERATION_IDLE;
     if (current_pos == desired_pos)
       desired_operation = COVER_OPERATION_IDLE;
+    else if (current_pos == AxaPosition::OPEN_MANUAL)
+      //close first
+      desired_operation = COVER_OPERATION_CLOSING;
     else if (current_pos < desired_pos)
     {
-      if (state_dirty && desired_pos!= AxaPosition::OPEN_FULL)
-      {
-        // close first to get to desired state
-        desired_operation = COVER_OPERATION_CLOSING;
-      }
       desired_operation = COVER_OPERATION_OPENING;
     }
     else if (current_pos > desired_pos)
     {
-      if (state_dirty && desired_pos!= AxaPosition::LOCKED)
-      {
-        // close first to get to desired state
-        desired_operation = COVER_OPERATION_OPENING;
-      }
       desired_operation = COVER_OPERATION_CLOSING;
     }
+    return desired_operation;
+  }
 
-    //apply desired_operation
+  void apply_operation(CoverOperation desired_operation)
+  {
+    if (current_operation != CoverOperation::COVER_OPERATION_IDLE)
+    {
+      send_cmd(AxaCommand::STOP);
+      delay(200);
+    }
+
     switch (desired_operation)
     {
       case COVER_OPERATION_OPENING:
       {
-        send_cmd(AxaCommand::OPEN);
-        current_operation = COVER_OPERATION_OPENING;
+        if (send_cmd(AxaCommand::OPEN) != AxaCode::OK)
+          break;
         last_state_change = millis();
         break;
       }
       case COVER_OPERATION_CLOSING:
       {
-        send_cmd(AxaCommand::OPEN);
-        delay(100);
-        send_cmd(AxaCommand::CLOSE);
-        current_operation = COVER_OPERATION_CLOSING;
+        if (send_cmd(AxaCommand::OPEN) != AxaCode::OK)
+          break;
+        delay(200);
+        if (send_cmd(AxaCommand::CLOSE) != AxaCode::OK)
+          break;
         last_state_change = millis();
         break;
       }
@@ -222,28 +238,63 @@ class EspAxaCover :
     }
   }
 
-  void poll_status(){
-    if ((millis() - lastread) < poll_delay_ms){
-      return;
-    }
-    lastread = millis();
+  AxaPosition poll_position(){
+    AxaCode poll_code = send_cmd(AxaCommand::STATUS);
 
-    // send STATUS to uart, wait a bit and read response
-    AxaCode axa_code = send_cmd(AxaCommand::STATUS);
+    switch(poll_code)
+    {
+      // Error responses:
+      case AxaCode::Error:
+      case AxaCode::Invalid:
+      // Invalid returns for STATUS
+      case AxaCode::OK:
+      case AxaCode::Firmware:
+      case AxaCode::Device:
+        return AxaPosition::UNKNOWN;
+        break;
 
-    if (axa_code == AxaCode::Unlocked and current_pos == AxaPosition::LOCKED){
-      desired_pos = current_pos = AxaPosition::OPEN_FULL; // assume fully opened by remote
-      state_dirty = true;
+      case AxaCode::Unlocked:
+      {
+        if (current_pos == AxaPosition::LOCKED)
+          return AxaPosition::OPEN_MANUAL;
+        break;
+      }
+      case AxaCode::Locked:
+      {
+        return AxaPosition::LOCKED;
+        break;
+      }
     }
-    if (axa_code == AxaCode::Locked){
-      desired_pos = current_pos = AxaPosition::LOCKED;
-    }
-    publish();
+    return current_pos;
   }
+
 
   void loop() override {
     uint32_t now = millis();
     int32_t time_since_last_change = now - last_state_change;
+    AxaPosition polled_pos = poll_position();
+    if (polled_pos == AxaPosition::UNKNOWN)
+      return;
+
+
+    ESP_LOGCONFIG(
+      "espaxa",
+      "loop: cop(%s), cpos(%i), dpos(%i), ppos(%i)",
+      cover_operation_to_str(current_operation),
+      int(current_pos),
+      int(desired_pos),
+      int(polled_pos)
+      );
+
+    if (last_state_change == 0)
+    {
+      current_pos = polled_pos;
+      last_state_change = now;
+      current_operation = update_operation(desired_pos);
+      apply_operation(current_operation);
+      return;
+    }
+
     if (current_operation == COVER_OPERATION_OPENING)
     {
       uint32_t step_time_ms = move_step_time_ms;
@@ -274,7 +325,6 @@ class EspAxaCover :
     if (current_operation == COVER_OPERATION_IDLE)
     {
       last_state_change = now;
-      poll_status();
     }
     else if (current_pos == desired_pos)
     {
@@ -282,20 +332,21 @@ class EspAxaCover :
       if (! (desired_pos == AxaPosition::OPEN_FULL || desired_pos == AxaPosition::LOCKED)){
         send_cmd(AxaCommand::STOP);
       }
-      // prevent status call right after state change
-      lastread = millis();
-
-      current_operation = COVER_OPERATION_IDLE;
       // Check if we are in the state we want to be
       // new command may have arrived
       // or we were moving from a dirty state to closed first
-      update_desired_operation();
-      publish();
+      current_operation = update_operation(desired_pos);
+      apply_operation(current_operation);
     }
+    current_operation = update_operation(desired_pos);
+    publish();
+    delay(250);
   }
 
   void publish()
   {
+    if (current_pos == AxaPosition::UNKNOWN)
+      return;
     this->position = denormalize_positon(current_pos);
     this->publish_state();
   }
